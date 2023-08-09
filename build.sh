b934fc3e96002e21403f03535435dc3b5bbe6778
unknown <nagatox60@gmail.com>
2023-08-09 19:34:57 +0100

    update build file

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -o errexit  # exit on error
 pip install -r ./newsfeed/requirements.txt
 
 python ./newsfeed/manage.py collectstatic --no-input
+python ./newsfeed/manage.py makemigrations
 python ./newsfeed/manage.py migrate
