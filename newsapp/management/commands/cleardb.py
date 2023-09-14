c16bc088203f0ffc443229cbad891ac421d5dbc6
unknown <nagatox60@gmail.com>
2023-09-14 22:09:58 +0100

    made change to managment command

--- newsapp/management/commands/cleardb.py
+++ newsapp/management/commands/cleardb.py
@@ -6,12 +6,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # deleting old news in the database
-        db_limit = 500
+        db_limit = 33
         count = News.objects.filter().count()
         print(f'news database entries is {count} in number')
         if count > db_limit:
             extra = count - db_limit
-            news = News.objects.filter(pk__in = News.objects.order_by('date').values_list('pk')[:extra]).delete()
-            print(f'news database entries reduced to {db_limit}')
+            print(f'extra news database entries is {extra}')
+            extra_news_id = News.objects.order_by('date')[:extra].values_list("id", flat=True)
+            News.objects.filter(pk__in=list(extra_news_id)).delete()
+            new_count = News.objects.filter().count()
+            print(f'news database entries reduced to {new_count}')
         else:
             print(f'news database entries not more than {db_limit}')
